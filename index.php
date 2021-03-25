e7bfffa25ad353f2b0b514db087d9d3a9992be39
Rasel Ahmed <gs57872@student.upm.edu.my>
2021-03-25 11:38:41 +0300

    Update

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php get_header(); ?>
 
-<div class="content-block">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="internal-content-wrap">
-                    <?php get_template_part('post-excerpt'); ?>
-                </div>
-            </div>         
+<div class="elementor-section elementor-section-boxed">
+    <div class="elementor-container">
+        <div class="internal-content-wrap">
+            <?php get_template_part('post-excerpt'); ?>
         </div>
     </div>
 </div>
